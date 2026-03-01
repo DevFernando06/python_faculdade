0a3917b287fbda081348f19e64642efabe281c53
Fernando <srnando4312@gmail.com>
2026-02-28 23:26:50 -0300

    feat: desafio02 - lista02

---
+++ AV01/lista02/desafio02.py
@@ -0,0 +1,18 @@
+numbers = []
+while True :
+    number = int(input("Adicione um número a lista: "))
+    numbers.append(number)
+
+    if len(numbers) < 2:
+        continue 
+
+    keep = input("Deseja continuar adicionando valores? [s | n]\n").lower()
+
+    if keep == "n" :
+        print("O maior valor da lista é: ", max(numbers))
+        print("O menor valor da lista é: ", min(numbers))
+        print("A soma de todos os valores lista é: ", sum(numbers))     
+        break
+
+
+    
